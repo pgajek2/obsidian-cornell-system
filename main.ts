2e7baf78571cd974e2734c30e58b49dab744d88a
Piotr PG Gajek <piotr.gajek95@gmail.com>
2023-02-26 21:31:12 +0100

    plugin rename

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
+export default class CornellSystem extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: CornellSystem;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CornellSystem) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
